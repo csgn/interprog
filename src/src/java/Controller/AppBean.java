5ec934cd47dc63f39653e9e677a00d7e4e2dd59b
csgn <dev.csgn@gmail.com>
2022-03-11 15:51:30 +0300

    Add form

--- src/src/java/Controller/AppBean.java
+++ src/src/java/Controller/AppBean.java
@@ -1,30 +1,12 @@
 package Controller;
 
-import Utils.PGConn;
-import Utils.PGConn.PGRow;
-import java.sql.Connection;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 import java.io.Serializable;
-import java.util.ArrayList;
 
 
 @Named("appBean")
 @SessionScoped
 public class AppBean implements Serializable {
-	private String test;
-
-	public AppBean() {
-	}
-	
-	public void setTest(String test) {
-		this.test = test;
-		Connection conn = PGConn.getConnection();
-		ArrayList<PGConn.PGRow> rows = PGConn.makeQuery("Select * from customer");
-
-		System.out.println(rows.toString());
-
-	}
-
 
 }
