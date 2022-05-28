230072c665e92a35c4a30092f2fba6a1cba62b1e
Metin Durmus <mtn.drms@outlook.com>
2022-05-28 14:00:56 +0300

    phone number validator

--- src/src/java/Validators/PhoneValidation.java
+++ src/src/java/Validators/PhoneValidation.java
@@ -1,10 +1,13 @@
 package Validators;
 
+import jakarta.faces.application.FacesMessage;
 import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.validator.FacesValidator;
 import jakarta.faces.validator.Validator;
 import jakarta.faces.validator.ValidatorException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -12,9 +15,25 @@ import jakarta.faces.validator.ValidatorException;
  */
 @FacesValidator("phoneValidator")
 public class PhoneValidation implements Validator {
+	
+	String patterns 
+      = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$" 
+      + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$" 
+      + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$";
+	
+	private Pattern patternObject;
+  private Matcher matcherObject;
+	
+	public PhoneValidation() {
+		patternObject = Pattern.compile(patterns);
+	}
 
 	@Override
 	public void validate(FacesContext fc, UIComponent uic, Object t) throws ValidatorException {
+		matcherObject = patternObject.matcher(t.toString());
+		if (!matcherObject.matches()) {
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,"Wrong Format", "Format of phone number is not valid");
+			throw new ValidatorException(message);
+		}
 	}
-	
 }
